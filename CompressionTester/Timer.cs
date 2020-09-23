c85e10c865d4f38b9c2a09f211656ad68775763c
Shawn Weisfeld <shawn@shawnweisfeld.com>
2020-09-22 20:49:50 -0500

    Initial commit

---
+++ CompressionTester/Timer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace CompressionTester
+{
+    public class Timer : IDisposable
+    {
+        Stopwatch _sw = new Stopwatch();
+
+        public Timer()
+        {
+            _sw.Start();
+        }
+
+        public void Dispose()
+        {
+            _sw.Stop();
+            Console.WriteLine($"\tDone in {_sw.ElapsedMilliseconds:N0} milliseconds");
+        }
+    }
+}
